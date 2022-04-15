dbbd7e986e505f0129d9b16e40137a46febda4e9
unknown <hatembendahmen@gmail.com>
2022-04-15 12:43:06 +0200

    config application.properties

--- profils/src/main/java/com/employes/profils/repository/ProfilRepository.java
+++ profils/src/main/java/com/employes/profils/repository/ProfilRepository.java
@@ -1,4 +1,4 @@
-package com.employes.profils.repos;
+package com.employes.profils.repository;
 
 import com.employes.profils.entities.Profil;
 import org.springframework.data.jpa.repository.JpaRepository;
